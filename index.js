785e2c7ba785d0dade27d8f5db00cf2582a68bd0
Rishika Jain <rishikajain@Rishikas-MacBook-Air.local>
2022-11-01 13:05:53 +0530

    changes

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
 const express = require("express");
 const app = express();
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 //const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
 
 require("dotenv").config();
 mongoose
-.connect(process.env.MONGO_URL)
-.then(() => console.log("DB Connection Successfull"))
-.catch((err) =>{
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DB Connection Successfull"))
+  .catch((err) => {
     console.log(err);
-});
-
-
+  });
 
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 
 app.listen(process.env.PORT || 8000, () => {
-    console.log("Backend server is running");
-})
+  console.log("Backend server is running");
+});
--- routes/auth.js
+++ routes/auth.js
@@ -1,26 +1,25 @@
 const router = require("express").Router();
-const User = require("../models/User")
+const User = require("../models/User");
 const CryptoJS = require("crypto-js");
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
 
-router.post("/register", async (req, res)=> {
-    const newUser = new User({
-        username: req.body.username,
-        email: req.body.email,
-        password: CryptoJS.AES.encrypt(
-          req.body.password,
-          process.env.PASS_SEC
-          ).toString(),
-    });
-    
-   try {
+router.post("/register", async (req, res) => {
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC
+    ).toString(),
+  });
+
+  try {
     const user = await newUser.save();
-    res.status(201).json(user)
-   } catch (err) {
-       res.status(500).json(err);
-   }
+    res.status(201).json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
-
 
 router.post("/login", async (req, res) => {
   const user = await User.findOne({
@@ -29,33 +28,29 @@ router.post("/login", async (req, res) => {
   if (!user) {
     return res.status(401).json("User Not Found");
   } else {
+    const hashedPassword = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.PASS_SEC
+    );
+    const originalpassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+    const inputPassword = req.body.password;
 
-  const hashedPassword = CryptoJS.AES.decrypt(
-    user.password,
-    process.env.PASS_SEC
-  );
-  const originalpassword = hashedPassword.toString(CryptoJS.enc.Utf8);
-  const inputPassword = req.body.password;
+    if (originalpassword != inputPassword) {
+      return res.status(401).json("Wrong credentials");
+    }
 
-  if (originalpassword != inputPassword) {
-    return res.status(401).json("Wrong credentials");
+    const accessToken = jwt.sign(
+      {
+        id: user._id,
+        isAdmin: user.isAdmin,
+      },
+      `${process.env.JWT_SEC}`,
+      { expiresIn: "3d" }
+    );
+
+    const { password, ...others } = user._doc;
+
+    res.status(200).json({ ...others, accessToken });
   }
-
-  const accessToken = jwt.sign(
-    {
-      id: user._id,
-      isAdmin: user.isAdmin,
-    },
-    `${process.env.JWT_SEC}`,
-    { expiresIn: "3d" }
-  );
-
-  const { password, ...others } = user._doc;
-
-  res.status(200).json({ ...others, accessToken });
-}
 });
-
-
-
 module.exports = router;
