a69e2f99acf6fa8efd7d53ead0dade26e963350f
Rishika Jain <rishikajain@Rishikas-MacBook-Air.local>
2022-11-02 18:36:05 +0530

    Modified code

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ const mongoose = require("mongoose");
 //const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
 
 require("dotenv").config();
+console.log("Mongo -", process.env.MONGO_URL);
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => console.log("DB Connection Successfull"))
@@ -16,6 +20,9 @@ mongoose
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
 
 app.listen(process.env.PORT || 8000, () => {
   console.log("Backend server is running");
--- routes/auth.js
+++ routes/auth.js
@@ -44,7 +44,7 @@ router.post("/login", async (req, res) => {
         id: user._id,
         isAdmin: user.isAdmin,
       },
-      `${process.env.JWT_SEC}`,
+      process.env.JWT_SEC,
       { expiresIn: "3d" }
     );
 
